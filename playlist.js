8149d7990139277f6e7c09a797aa5ab211bc81cc
paulkg4u <paulkottalil20@gmail.com>
2014-02-02 01:10:15 +0530

    second commit

---
+++ playlist.js
@@ -0,0 +1,13 @@
+window.onload=init;
+function init(){
+	var button =document.getElementById("addButton");
+	button.onclick=handleButtonClick;
+}
+function handleButtonClick(){
+	var textInput=document.getElementById("songText");
+	var songName=textInput.value;
+	var li = document.createElement("li");
+	li.innerHTML=songName;
+	var ul = document.getElementById("playlist");
+	ul.appendChild(li);
+}
